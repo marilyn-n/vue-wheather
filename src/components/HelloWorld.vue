76809885062d0cff30083cfe41a7cf5bb1f6c41c
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-06-10 15:36:44 -0500

    search bar made it!

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="main">
+  <div class="container"> 
+    <div class="row">
+    <div class="col"> 
+      <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="Insert Country" aria-label="Recipient's username" aria-describedby="basic-addon2">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button">Go</button>
+        </div>
+      </div>
+     </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-    }
+  name: "HelloWorld",
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <style scoped>
